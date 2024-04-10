0b2077f8b43570320cce9874b7b88ea6785b31d6
herrk <herrkarstone@gmail.com>
2024-04-10 16:03:20 +0300

    .gitignore added

---
+++ products/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+from products.models import Product, ProductCategory
+
+# Create your views here.
+def index(request):
+    context = {
+        "title": 'My Store',
+    }
+    return render(request, 'products/index.html', context=context)
+
+def products(request):
+    context = {
+        'title': 'My Store - Каталог',
+        'products': Product.objects.all(),
+        'category': ProductCategory.objects.all(),
+    }
+    return render(request=request, template_name='products/products.html', context=context)
