d5d3458a84a12168fe583f0d25bced10034ae3b0
herrk <herrkarstone@gmail.com>
2024-04-12 18:29:50 +0300

    Login and registration aded with templates, works uncorrect

--- users/views.py
+++ users/views.py
@@ -1,13 +1,38 @@
-from django.shortcuts import render
-from users.forms import UserLoginForm
-#from users.models import User
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
+from users.forms import UserLoginForm, UserRegistrationForm
+
+# from users.models import User
+
 
 def login(request):
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse("index"))
+    else:
+        form = UserLoginForm()
     context = {
-        "form": UserLoginForm(),
-        }
-    return render(request, 'users/login.html', context)
+        "form": form,
+    }
+    return render(request, "users/login.html", context)
 
 
 def registration(request):
-    return render(request, 'users/registration.html')
+    if request.method == "POST":
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("users:login"))
+    else:
+        form = UserRegistrationForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "users/registration.html", context)
