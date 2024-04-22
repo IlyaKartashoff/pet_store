4afac36890f1ac1f4a31aa4da513dcc285d992e9
herrk <herrkarstone@gmail.com>
2024-04-22 14:44:00 +0300

    basket edited, BasketQuerySet as manager added

--- products/models.py
+++ products/models.py
@@ -22,12 +22,23 @@ class Product(models.Model):
         return f'Продукт {self.name} | Ост. - {self.quantity}'
 
 
+class BasketQuerrySet(models.QuerySet):
+    def total_sum(self):
+        return sum(basket.sum() for basket in self)
+
+    def total_quantity(self):
+        return sum(basket.quantity for basket in self)
+
 class Basket(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
     product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
     quantity = models.SmallIntegerField(default=0)
     created_timestamp = models.DateTimeField(auto_now_add=True)
 
+    objects = BasketQuerrySet.as_manager()
+
     def __str__(self):
         return f"Корзина для {self.user.username} | Продукт: {self.product.name}"
     
+    def sum(self):
+        return self.product.price * self.quantity
