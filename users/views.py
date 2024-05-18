b96bd3432fc837675958457df6549a2313be1f4e
herrk <herrkarstone@gmail.com>
2024-05-18 18:22:39 +0300

    context processor for variable baskets added

--- users/views.py
+++ users/views.py
@@ -6,56 +6,53 @@ from django.views.generic.base import TemplateView
 from django.views.generic import CreateView, UpdateView
 from django.contrib.messages.views import SuccessMessageMixin
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
-from products.models import Basket
 from users.models import User, EmailVerification
 from common.views import TitleMixin
 
 
-class UserLoginView(TitleMixin ,LoginView):
-    template_name = 'users/login.html'
+class UserLoginView(TitleMixin, LoginView):
+    template_name = "users/login.html"
     form_class = UserLoginForm
     title = "Store - Авторизация"
 
 
-class UserRegistrationView(TitleMixin ,SuccessMessageMixin,CreateView):
+class UserRegistrationView(TitleMixin, SuccessMessageMixin, CreateView):
     """Контроллер для регистрации пользователя"""
+
     model = User
     title = "Store - Регистрация"
     form_class = UserRegistrationForm
-    template_name = 'users/registration.html'
-    success_url =reverse_lazy('users:login')
-    success_message = 'Вы успешно зарегистрированы!'
-    
-    
+    template_name = "users/registration.html"
+    success_url = reverse_lazy("users:login")
+    success_message = "Вы успешно зарегистрированы!"
 
-class UserProfileView(TitleMixin ,UpdateView):
+
+class UserProfileView(TitleMixin, UpdateView):
     """Контроллер для профиля, отображения и обновления информации пользователя"""
+
     model = get_user_model()
     form_class = UserProfileForm
-    template_name = 'users/profile.html'
-    title = 'Store - Личный кабинет'
+    template_name = "users/profile.html"
+    title = "Store - Личный кабинет"
 
     def get_success_url(self):
-        return reverse_lazy('users:profile', args=(self.object.id,))
-    
-    def get_context_data(self, **kwargs):
-        context = super(UserProfileView, self).get_context_data()
-        context['baskets'] = Basket.objects.filter(user=self.object)
-        return context
-    
+        return reverse_lazy("users:profile", args=(self.object.id,))
+
 
 class EmailVerificationView(TitleMixin, TemplateView):
-    title = 'Store - Подтверждение электронной почты'
-    template_name = 'users/email_verification.html'
+    title = "Store - Подтверждение электронной почты"
+    template_name = "users/email_verification.html"
 
     def get(self, request, *args, **kwargs):
-        code = kwargs['code']
-        user = User.objects.get(email=kwargs['email'])
+        code = kwargs["code"]
+        user = User.objects.get(email=kwargs["email"])
         email_verifications = EmailVerification.objects.filter(user=user, code=code)
-        if email_verifications.exists() and not email_verifications.first().is_expired():
+        if (
+            email_verifications.exists()
+            and not email_verifications.first().is_expired()
+        ):
             user.is_verificated_email = True
             user.save()
             return super(EmailVerificationView, self).get(request, *args, **kwargs)
         else:
-            return HttpResponseRedirect(reverse('index'))
-        
+            return HttpResponseRedirect(reverse("index"))
