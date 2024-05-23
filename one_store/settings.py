e34af5c5deccc82ead8a0108c4a1922abc9ce5e0
herrk <herrkarstone@gmail.com>
2024-05-23 21:56:30 +0300

    migrate postgresql

--- one_store/settings.py
+++ one_store/settings.py
@@ -97,8 +97,12 @@ WSGI_APPLICATION = 'one_store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'storedb',
+        'USER': 'store',
+        'PASSWORD': 'store',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
